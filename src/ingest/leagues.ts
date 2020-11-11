58af0b63bfd9092b0cc71663b341e5fd955d42e9
Colin Cheung <ColinCee@users.noreply.github.com>
2020-11-11 22:35:47 +0000

    update ingest

--- src/ingest/leagues.ts
+++ src/ingest/leagues.ts
@@ -1,3 +1,4 @@
+import { promises } from "fs-extra";
 import sleep from "sleep-promise";
 import { LeagueApi } from "twisted/dist/apis/lol/league/league";
 import { Regions } from "twisted/dist/constants";
@@ -12,45 +13,66 @@ import { TftSummoner } from "../../models/TftSummoner";
 import { Postgres } from "../api/postgres";
 import { releaseKey } from "../api/riot/keyManager";
 import { getSummoner, getTftApi } from "../api/riot/riot";
-
-export const ingestMasterSummoners = async () => {
+import { Op } from "sequelize";
+export const ingestHighEloLeagues = async () => {
   const [tftApi, key] = await getTftApi();
-  const league = await tftApi.League.getMasterLeague(Regions.EU_WEST);
+  const masterLeague = await tftApi.League.getMasterLeague(Regions.EU_WEST);
+  await sleep(1200);
+  const grandMasterLeague = await tftApi.League.getGrandMasterLeague(
+    Regions.EU_WEST
+  );
+  await sleep(1200);
+  const challengerLeague = await tftApi.League.getChallengerLeague(
+    Regions.EU_WEST
+  );
+
   await releaseKey(key);
-  await processLeague(league.response);
+  const eloTime = new Date();
+
+  const leagues = [masterLeague, grandMasterLeague, challengerLeague];
+  for await (const league of leagues) {
+    await processLeague(league.response, eloTime);
+  }
 };
 
-const processLeague = async (league: LeagueListDTO) => {
-  const eloTime = new Date();
+const processLeague = async (league: LeagueListDTO, eloTime: Date) => {
   let count = 0;
+  // const summonerNames = league.entries.map((entry) => entry.summonerName);
+  // const tftSummoners = await TftSummoner.findAll({
+  //   where: {
+  //     summonerName: {
+  //       [Op.in]: summonerNames,
+  //     },
+  //   },
+  // });
+  // const summonersInDb = tftSummoners.map((summoner) => summoner.summonerName);
+  // const summonersNotInDb = league.entries.filter(
+  //   (entry) => !summonersInDb.includes(entry.summonerName)
+  // );
   for await (const entry of league.entries) {
-    console.log(`${count}/${league.entries.length}`);
+    console.log(`${league.tier} - ${count}/${league.entries.length}`);
     count += 1;
-    const summCount = await TftSummoner.count({
-      where: { summonerName: entry.summonerName },
-    });
-    if (summCount > 0) {
-      continue;
-    }
+
     const [summonerDto, key] = await getSummonerByName(entry.summonerName);
     if (!summonerDto) {
       continue;
     }
     const { id, name, puuid } = summonerDto.response;
     const transaction = await Postgres.newTransaction();
-    const summoner = await TftSummoner.create(
-      {
+    const [summoner] = await TftSummoner.findOrCreate({
+      where: {
         summonerName: name,
         summonerRegion: Regions.EU_WEST,
       },
-      { transaction }
-    );
+      transaction,
+    });
     const tftElo = await TftElo.findOne({
       where: {
-        tier: "MASTER",
+        tier: league.tier,
         rank: entry.rank,
         lp: entry.leaguePoints,
       },
+      transaction,
     });
     await TftSummonerElo.create(
       {
@@ -67,15 +89,15 @@ const processLeague = async (league: LeagueListDTO) => {
       },
       transaction,
     });
-    await TftSummonerApiKey.create(
-      {
+    await TftSummonerApiKey.findOrCreate({
+      where: {
         tftSummonerId: summoner.tftSummonerId,
         tftApiKeyId: tftApiKey.tftApiKeyId,
         encryptedPlayerUuid: puuid,
         encryptedSummonerId: id,
       },
-      { transaction }
-    );
+      transaction,
+    });
     await transaction.commit();
   }
 };
