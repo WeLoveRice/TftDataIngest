9c82d94c2d65b19dd400f5e2ee9bbf413a42fefb
Colin Cheung <ColinCee@users.noreply.github.com>
2020-11-13 18:56:55 +0000

    refactor keymanager

--- src/api/riot/keyManager.ts
+++ src/api/riot/keyManager.ts
@@ -1,42 +1,52 @@
 import fs from "fs-extra";
 import path from "path";
-import sleep from "sleep-promise";
-import { createLogger } from "../../Logger";
 import { Redis } from "../redis";
 
 const keysFile = path.resolve(__dirname, "..", "..", "..", "riot-api-keys.txt");
 const REDIS_RIOT_KEYS = "RIOT_KEYS";
 
-export const addKeysToRedis = async () => {
+export const initKeys = async () => {
   const data = await fs.readFile(keysFile, "utf-8");
-  const keys = data.split("\n").filter((value) => value);
+  const apiKeys = data.split("\n").filter((value) => value);
   const redis = await Redis.getConnection();
   await redis.flushall();
 
-  const keysInserted = await redis.rpush(REDIS_RIOT_KEYS, ...keys);
-  console.log(await redis.lrange(REDIS_RIOT_KEYS, 0, -1));
-  if (keysInserted == 0) {
-    throw new Error("Could not add keys to redis");
+  for await (const key of apiKeys) {
+    await redis.rpush(key, key);
   }
+
+  setInterval(async () => await keyTransfer(apiKeys), 1000);
+};
+
+const keyTransfer = async (keys: string[]) => {
+  const redis = await Redis.getConnection();
+  Promise.all(
+    keys.map(async (key) => {
+      const apiKey = await redis.lpop(key);
+      if (apiKey) {
+        await redis.rpush(REDIS_RIOT_KEYS, apiKey);
+      }
+    })
+  );
 };
 
 export const getKey = async () => {
   const redis = await Redis.connect();
   const [redisKey, apiKey] = await redis.blpop(REDIS_RIOT_KEYS, 0);
 
-  if (await redis.get(apiKey)) {
-    console.log(`found key ${apiKey}, waiting...`);
-    await sleep(1200);
-  }
-
-  await redis.quit();
   return apiKey;
 };
 
-export const releaseKey = async (key: string) => {
+export const getSpecificKey = async (apiKey: string) => {
+  const redis = await Redis.connect();
+  const [redisKey, key] = await redis.blpop(apiKey, 0);
+
+  return key;
+};
+
+export const releaseKey = async (apiKey: string) => {
   setTimeout(async () => {
     const redis = await Redis.connect();
-    await redis.rpush(REDIS_RIOT_KEYS, key);
-    await redis.quit();
+    await redis.rpush(apiKey, apiKey);
   }, 1200);
 };
