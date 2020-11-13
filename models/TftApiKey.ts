8380e1db95a6991e9fbc7c68de9864a590d6db04
Colin Cheung <ColinCee@users.noreply.github.com>
2020-11-13 17:39:46 +0000

    fix tftapikey

--- models/TftApiKey.ts
+++ models/TftApiKey.ts
@@ -1,53 +1,57 @@
 /* jshint indent: 2 */
 
-import { DataTypes, Model, Sequelize } from 'sequelize';
+import { DataTypes, Model, Optional, Sequelize } from "sequelize";
 
 export interface TftApiKeyAttributes {
-  tftApiKeyId?: number;
-  riotApiKey?: string;
+  tftApiKeyId: number;
+  riotApiKey: string;
 }
 
-export class TftApiKey extends Model<TftApiKeyAttributes, TftApiKeyAttributes> implements TftApiKeyAttributes {
-  tftApiKeyId?: number;
-  riotApiKey?: string;
+// Some attributes are optional in `User.build` and `User.create` calls
+type TftApiKeyCreationAttributes = Optional<TftApiKeyAttributes, "tftApiKeyId">;
+
+export class TftApiKey
+  extends Model<TftApiKeyAttributes, TftApiKeyCreationAttributes>
+  implements TftApiKeyAttributes {
+  tftApiKeyId!: number;
+  riotApiKey!: string;
 
   static initModel(sequelize: Sequelize) {
-    TftApiKey.init({
-    tftApiKeyId: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      field: 'tft_api_key_id'
-    },
-    riotApiKey: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      unique: "riot_api_key_unq",
-      field: 'riot_api_key'
-    }
-  }, {
-    sequelize,
-    tableName: 'tft_api_key',
-    schema: 'public',
-    timestamps: false,
-    indexes: [
+    TftApiKey.init(
       {
-        name: "riot_api_key_unq",
-        unique: true,
-        fields: [
-          { name: "riot_api_key" },
-        ]
+        tftApiKeyId: {
+          autoIncrement: true,
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          field: "tft_api_key_id",
+        },
+        riotApiKey: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+          unique: "riot_api_key_unq",
+          field: "riot_api_key",
+        },
       },
       {
-        name: "tft_api_key_pkey",
-        unique: true,
-        fields: [
-          { name: "tft_api_key_id" },
-        ]
-      },
-    ]
-  });
-  return TftApiKey;
+        sequelize,
+        tableName: "tft_api_key",
+        schema: "public",
+        timestamps: false,
+        indexes: [
+          {
+            name: "riot_api_key_unq",
+            unique: true,
+            fields: [{ name: "riot_api_key" }],
+          },
+          {
+            name: "tft_api_key_pkey",
+            unique: true,
+            fields: [{ name: "tft_api_key_id" }],
+          },
+        ],
+      }
+    );
+    return TftApiKey;
   }
 }
